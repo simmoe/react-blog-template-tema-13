7aceb1b408ce2c305c1743062ee0ccf65f88716b
Simon Moe <simmoe@gmail.com>
2020-02-05 14:58:46 +0100

    Updates

---
+++ precache-manifest.0fe01160352179220d707a07ab961dce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013eb25117f06783b3c324478f08de00",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "7a1dc51753d2bd31da3e",
+    "url": "/blog/static/css/main.3d1755bc.chunk.css"
+  },
+  {
+    "revision": "85b56c3d0909e59b9751",
+    "url": "/blog/static/js/2.a94e9df0.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/blog/static/js/2.a94e9df0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a1dc51753d2bd31da3e",
+    "url": "/blog/static/js/main.296bf4d2.chunk.js"
+  },
+  {
+    "revision": "cbfd563c8600f0c401a0",
+    "url": "/blog/static/js/runtime-main.3e812e36.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-blog-template-tema-13/precache-manifest.00eb053333742ad510772961df6ca97e.js"
+  "/blog/precache-manifest.0fe01160352179220d707a07ab961dce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-blog-template-tema-13/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
